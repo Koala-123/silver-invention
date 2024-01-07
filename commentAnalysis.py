060b54dd3f659b5951fa89022c8af95ed6bebea1
Koala-123 <138291177+Koala-123@users.noreply.github.com>
2024-01-07 06:49:05 +0000

    commit

--- commentAnalysis.py
+++ commentAnalysis.py
@@ -7,4 +7,43 @@ import emoji
 # Analyze the sentiments of the comment
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+import matplotlib.pyplot as plt
+
 API_KEY = 'AIzaSyDIdrPdFFWtnnd1e8sMY2Kf5GrzVa2LEYk'
+
+youtube=build('youtube','v3',developerKey=API_KEY)
+
+video_id=input('Enter YouTuble video URL: ')[-11:]
+print("video_id: "+video_id)
+
+video_response=youtube.videos().list(
+    part='snippet',
+    id=video_id
+).execute()
+
+video_snippet=video_response['items'][0]['snippet']
+uploader_channel_id=video_snippet['channelId']
+print('channel id: '+uploader_channel_id)
+
+print("Fetching comments...")
+comments=[]
+nextPageToken=None
+
+while len(comments)<600:
+    request=youtube.commentThreads().list(
+        part='snippet'
+        videoId=video_id
+        maxResults=100
+        pageToken=nextPageToken
+    )
+    response=request.execute()
+    for item in response['items']:
+        comment=item['snippet']['topLevelComment']['snippet']
+        if comment['authorChannelId']['value']!=uploader_channel_id:
+            comments.append(comment['textDisplay'])
+    nextPageToken=response.get('nextPageToken')
+
+    if not nextPageToken:
+        break
+
+    comments[:5]
